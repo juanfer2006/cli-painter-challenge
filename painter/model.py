bf3985efb348017b4833a599cc8d2de25d1f7648
phpg21 <ppalaciosgoez@gmail.com>
2024-08-06 10:46:09 -0500

    point

--- painter/model.py
+++ painter/model.py
@@ -1,1 +1,10 @@
 # TODO: Add code here
+class Point:
+
+    def __init__(self, x: float, y: float):
+        self.x: float = x
+        self.y: float = y
+
+class Circle:
+
+    def __init__(self, center: Point, : float):
